8297b91ceddc6e82fa926b375d918f05a8e16317
Arifa Rubab <160140406+arifarubab@users.noreply.github.com>
2024-05-30 22:16:18 +0500

    Add files via upload

---
+++ game.ts
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+let guess = 8;
+
+let guessNumber = await inquirer.prompt(
+    {
+        message:"Guess your Number",
+        type:"number",
+        name:"yourGuess"
+}
+
+);
+if(guessNumber.yourGuess === guess)
+    {
+        console.log("Congratulations!!! Your guess is right..........");
+
+}
+else{
+    console.log("Opss!!! Your guess is wrong");
+}
